efc73942f6f1bbc5446e915c24a78d11bb7f2d2b
Emilio <redorav@gmail.com>
2017-02-19 20:26:45 +0000

    * Fix typo in matrix swizzling

--- src/swizzle/hlsl++_matrix_row0_2.h
+++ src/swizzle/hlsl++_matrix_row0_2.h
@@ -44,40 +44,40 @@ component4<1, 1, 1, 1> _m01_m01_m01_m01;
 // _NM
 // -------------------------------
 
-component1<1> _m12;
+component1<1> _12;
 
-component2<0, 1> _m11_m12;
-component2<1, 0> _m12_m11;
-component2<1, 1> _m12_m12;
+component2<0, 1> _11_12;
+component2<1, 0> _12_11;
+component2<1, 1> _12_12;
 
-component3<0, 0, 1> _m11_m11_m12;
-component3<0, 1, 0> _m11_m12_m11;
-component3<0, 1, 1> _m11_m12_m12;
+component3<0, 0, 1> _11_11_12;
+component3<0, 1, 0> _11_12_11;
+component3<0, 1, 1> _11_12_12;
 
-component3<1, 0, 0> _m12_m11_m11;
-component3<1, 0, 1> _m12_m11_m12;
-component3<1, 1, 0> _m12_m12_m11;
-component3<1, 1, 1> _m12_m12_m12;
+component3<1, 0, 0> _12_11_11;
+component3<1, 0, 1> _12_11_12;
+component3<1, 1, 0> _12_12_11;
+component3<1, 1, 1> _12_12_12;
 
-component4<0, 0, 0, 1> _m11_m11_m11_m12;
+component4<0, 0, 0, 1> _11_11_11_12;
 
-component4<0, 0, 1, 0> _m11_m11_m12_m11;
-component4<0, 0, 1, 1> _m11_m11_m12_m12;
+component4<0, 0, 1, 0> _11_11_12_11;
+component4<0, 0, 1, 1> _11_11_12_12;
 
-component4<0, 1, 0, 0> _m11_m12_m11_m11;
-component4<0, 1, 0, 1> _m11_m12_m11_m12;
+component4<0, 1, 0, 0> _11_12_11_11;
+component4<0, 1, 0, 1> _11_12_11_12;
 
-component4<0, 1, 1, 0> _m11_m12_m12_m11;
-component4<0, 1, 1, 1> _m11_m12_m12_m12;
+component4<0, 1, 1, 0> _11_12_12_11;
+component4<0, 1, 1, 1> _11_12_12_12;
 
-component4<1, 0, 0, 0> _m12_m11_m11_m11;
-component4<1, 0, 0, 1> _m12_m11_m11_m12;
+component4<1, 0, 0, 0> _12_11_11_11;
+component4<1, 0, 0, 1> _12_11_11_12;
 
-component4<1, 0, 1, 0> _m12_m11_m12_m11;
-component4<1, 0, 1, 1> _m12_m11_m12_m12;
+component4<1, 0, 1, 0> _12_11_12_11;
+component4<1, 0, 1, 1> _12_11_12_12;
 
-component4<1, 1, 0, 0> _m12_m12_m11_m11;
-component4<1, 1, 0, 1> _m12_m12_m11_m12;
+component4<1, 1, 0, 0> _12_12_11_11;
+component4<1, 1, 0, 1> _12_12_11_12;
 
-component4<1, 1, 1, 0> _m12_m12_m12_m11;
-component4<1, 1, 1, 1> _m12_m12_m12_m12;
+component4<1, 1, 1, 0> _12_12_12_11;
+component4<1, 1, 1, 1> _12_12_12_12;
