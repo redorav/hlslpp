5ac52bac83c0ba4589a82922de74ac92c3a44b65
Evgeny Gorodetsky <evsgor@gmail.com>
2021-01-02 14:20:23 +0300

    Add scale matrix unit tests and more rotation matrix tests

--- src/hlsl++_unit_tests_matrix_transform.cpp
+++ src/hlsl++_unit_tests_matrix_transform.cpp
@@ -7,22 +7,42 @@ void RunUnitTestsMatrixTransform()
 	using namespace hlslpp_unit;
 
 	const float pi = std::acos(-1.f);
-	const float tolerance(1e-7);
+	const float sq2 = std::sqrt(2.f) / 2.f;
+	const float tolerance = 1e-7f;
 
-	// 2D Rotation
-
+	const float2 vec2d_a1(1.f, 1.f);
 	const float2 vec2d_x1(1.f, 0.f);
-	const float  sq2 = std::sqrt(2.f) / 2.f;
+	const float2 vec2d_y1(0.f, 1.f);
 
-	eq(mul(float2x2_rotation(pi / 2.f), vec2d_x1), 0.f, 1.f, tolerance);
-	eq(mul(float2x2_rotation(pi / 4.f), vec2d_x1), sq2, sq2, tolerance);
-	eq(mul(float2x2_rotation(pi * 3.f / 4.f), vec2d_x1), -sq2, sq2, tolerance);
-
-	// 3D Rotation
-
+	const float3 vec3d_a1(1.f, 1.f, 1.f);
 	const float3 vec3d_x1(1.f, 0.f, 0.f);
 	const float3 vec3d_y1(0.f, 1.f, 0.f);
 	const float3 vec3d_z1(0.f, 0.f, 1.f);
+
+	// 2D Scale
+
+	eq(mul(float2x2_scale(2.f), vec2d_a1), 2.f, 2.f, tolerance);
+	eq(mul(float2x2_scale(2.f, 3.f), vec2d_a1), 2.f, 3.f, tolerance);
+	eq(mul(float2x2_scale(float2(4.f, 5.f)), vec2d_a1), 4.f, 5.f, tolerance);
+
+	// 3D Scale
+
+	eq(mul(float3x3_scale(2.f), vec3d_a1), 2.f, 2.f, 2.f, tolerance);
+	eq(mul(float3x3_scale(2.f, 3.f, 4.f), vec3d_a1), 2.f, 3.f, 4.f, tolerance);
+	eq(mul(float3x3_scale(float3(4.f, 5.f, 6.f)), vec3d_a1), 4.f, 5.f, 6.f, tolerance);
+
+	// 2D Rotation
+
+	eq(mul(float2x2_rotation(pi / 2.f), vec2d_x1), 0.f, 1.f, tolerance);
+	eq(mul(float2x2_rotation(pi / 2.f), vec2d_y1), -1.f, 0.f, tolerance);
+
+	eq(mul(float2x2_rotation(pi / 4.f), vec2d_x1), sq2, sq2, tolerance);
+	eq(mul(float2x2_rotation(pi / 4.f), vec2d_y1), -sq2, sq2, tolerance);
+
+	eq(mul(float2x2_rotation(pi * 3.f / 4.f), vec2d_x1), -sq2, sq2, tolerance);
+	eq(mul(float2x2_rotation(pi * 3.f / 4.f), vec2d_y1), -sq2, -sq2, tolerance);
+
+	// 3D Rotation
 
 	eq(mul(float3x3_rotation(vec3d_x1, -pi / 2.f), vec3d_y1), 0.f, 0.f, 1.f, tolerance);
 	eq(mul(float3x3_rotation(vec3d_y1, -pi / 2.f), vec3d_z1), 1.f, 0.f, 0.f, tolerance);
