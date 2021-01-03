e23af5a96d62b23b3a565ea10d81d97829b8a50b
Evgeny Gorodetsky <evsgor@gmail.com>
2021-01-03 19:56:04 +0300

    Add implicit operator casting floatN vectors to bool for checking vectors equality with if (v1 == v2)

--- include/hlsl++_vector_float.h
+++ include/hlsl++_vector_float.h
@@ -761,6 +761,8 @@ namespace hlslpp
 
 		hlslpp_inline operator float() const { return f32[0]; }
 
+		hlslpp_inline explicit operator bool() const  { return _hlslpp_all1_ps(vec); }
+
 		union
 		{
 			n128 vec;
@@ -791,6 +793,8 @@ namespace hlslpp
 		hlslpp_inline float2(const int2& i);
 
 		hlslpp_inline float2(const uint2& i);
+
+		hlslpp_inline explicit operator bool() const { return _hlslpp_all2_ps(vec); }
 
 		union
 		{
@@ -827,6 +831,8 @@ namespace hlslpp
 		hlslpp_inline float3(const int3& i);
 
 		hlslpp_inline float3(const uint3& i);
+
+		hlslpp_inline explicit operator bool() const { return _hlslpp_all3_ps(vec); }
 
 		union
 		{
@@ -868,6 +874,8 @@ namespace hlslpp
 		hlslpp_inline float4(const int4& i);
 
 		hlslpp_inline float4(const uint4& i);
+
+		hlslpp_inline explicit operator bool() const { return _hlslpp_all4_ps(vec); }
 
 		union
 		{
