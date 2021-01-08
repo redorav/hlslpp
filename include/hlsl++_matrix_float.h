ef3a67b7c387bc69dbaeb2a1488db4f0486865c8
Emilio López <redorav@gmail.com>
2021-01-08 20:01:20 +0000

    Make natvis work in both AVX and SSE mode for float4x4

--- include/hlsl++_matrix_float.h
+++ include/hlsl++_matrix_float.h
@@ -401,7 +401,7 @@ namespace hlslpp
 		union
 		{
 			n256 vec0;
-			float f32_0[8];
+			float f32_256_0[8];
 
 			HLSLPP_WARNING_ANONYMOUS_STRUCT_UNION_BEGIN
 			struct
@@ -428,7 +428,7 @@ namespace hlslpp
 		union
 		{
 			n256 vec1;
-			float f32_1[8];
+			float f32_256_1[8];
 
 			HLSLPP_WARNING_ANONYMOUS_STRUCT_UNION_BEGIN
 			struct
@@ -474,7 +474,7 @@ namespace hlslpp
 		union
 		{
 			n128 vec0;
-			float f32_0[4];
+			float f32_128_0[4];
 			#include "swizzle/hlsl++_matrix_row0_1.h"
 			#include "swizzle/hlsl++_matrix_row0_2.h"
 			#include "swizzle/hlsl++_matrix_row0_3.h"
@@ -484,7 +484,7 @@ namespace hlslpp
 		union
 		{
 			n128 vec1;
-			float f32_1[4];
+			float f32_128_1[4];
 			#include "swizzle/hlsl++_matrix_row1_1.h"
 			#include "swizzle/hlsl++_matrix_row1_2.h"
 			#include "swizzle/hlsl++_matrix_row1_3.h"
@@ -494,7 +494,7 @@ namespace hlslpp
 		union
 		{
 			n128 vec2;
-			float f32_2[4];
+			float f32_128_2[4];
 			#include "swizzle/hlsl++_matrix_row2_1.h"
 			#include "swizzle/hlsl++_matrix_row2_2.h"
 			#include "swizzle/hlsl++_matrix_row2_3.h"
@@ -504,7 +504,7 @@ namespace hlslpp
 		union
 		{
 			n128 vec3;
-			float f32_3[4];
+			float f32_128_3[4];
 			#include "swizzle/hlsl++_matrix_row3_1.h"
 			#include "swizzle/hlsl++_matrix_row3_2.h"
 			#include "swizzle/hlsl++_matrix_row3_3.h"
