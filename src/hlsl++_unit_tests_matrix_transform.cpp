62c69a55c234c80ab3b40a7aa1a47f9fadd43161
Evgeny Gorodetsky <evsgor@gmail.com>
2021-01-11 23:02:44 +0300

    Add matrix layout and coordinates macro-checks to transform unit tests

--- src/hlsl++_unit_tests_matrix_transform.cpp
+++ src/hlsl++_unit_tests_matrix_transform.cpp
@@ -25,6 +25,8 @@ void RunUnitTestsMatrixTransform()
 	const float4 vec4d_y1(0.0f, 1.0f, 0.0f, 1.0f);
 	const float4 vec4d_z1(0.0f, 0.0f, 1.0f, 1.0f);
 
+#if HLSLPP_LOGICAL_LAYOUT == HLSLPP_LOGICAL_LAYOUT_ROW_MAJOR
+
 	// Scaling
 
 	eq(mul(vec2d_a1, float2x2::scale(2.0f)), 2.0f, 2.0f, tolerance);
@@ -44,6 +46,16 @@ void RunUnitTestsMatrixTransform()
 
 	eq(mul(vec3d_a1, float3x3::scale(float3(4.0f, 5.0f, 6.0f))), 4.0f, 5.0f, 6.0f, tolerance);
 	eq(mul(vec4d_a1, float4x4::scale(float3(4.0f, 5.0f, 6.0f))), 4.0f, 5.0f, 6.0f, 1.0f, tolerance);
+
+	// Translation
+
+	eq(mul(vec3d_a1, float3x3::translation(2.0f, 3.0f)), 3.0f, 4.0f, 1.0f, tolerance);
+	eq(mul(vec3d_a1, float3x3::translation(float2(2.0f, 3.0f))), 3.0f, 4.0f, 1.0f, tolerance);
+
+	eq(mul(vec4d_a1, float4x4::translation(2.0f, 3.0f, 4.0f)), 3.0f, 4.0f, 5.0f, 1.0f, tolerance);
+	eq(mul(vec4d_a1, float4x4::translation(float3(2.0f, 3.0f, 4.0f))), 3.0f, 4.0f, 5.0f, 1.0f, tolerance);
+
+#if HLSLPP_COORDINATES == HLSLPP_COORDINATES_LEFT_HANDED
 
 	// Rotation
 
@@ -110,14 +122,6 @@ void RunUnitTestsMatrixTransform()
 	eq(mul(vec3d_x1, float3x3::rotation_axis(vec3d_z1, pi * 3.0f / 4.0f)), -sq2, sq2, 0.0f, tolerance);
 	eq(mul(vec4d_x1, float4x4::rotation_axis(vec3d_z1, pi * 3.0f / 4.0f)), -sq2, sq2, 0.0f, 1.0f, tolerance);
 
-	// Translation
-
-	eq(mul(vec3d_a1, float3x3::translation(2.0f, 3.0f)), 3.0f, 4.0f, 1.0f, tolerance);
-	eq(mul(vec3d_a1, float3x3::translation(float2(2.0f, 3.0f))), 3.0f, 4.0f, 1.0f, tolerance);
-
-	eq(mul(vec4d_a1, float4x4::translation(2.0f, 3.0f, 4.0f)), 3.0f, 4.0f, 5.0f, 1.0f, tolerance);
-	eq(mul(vec4d_a1, float4x4::translation(float3(2.0f, 3.0f, 4.0f))), 3.0f, 4.0f, 5.0f, 1.0f, tolerance);
-
 	// World to View coordinates transformation
 
 	const float3 view_position(1.0f, 0.0f, 0.0f);
@@ -141,4 +145,8 @@ void RunUnitTestsMatrixTransform()
 
 	eq(mul(vec4d_view2, float4x4::perspective(projection(persp_frustrum, zclip::zero))), 0.001f, 0.001f, 1.0f, 1.0f, tolerance);
 	eq(mul(vec4d_view2, float4x4::perspective(projection(persp_frustrum, zclip::negative_one))), 0.001f, 0.001f, 1.0f, 1.0f, tolerance);
+
+#endif // HLSLPP_COORDINATES == HLSLPP_COORDINATES_LEFT_HANDED
+
+#endif // HLSLPP_LOGICAL_LAYOUT == HLSLPP_LOGICAL_LAYOUT_ROW_MAJOR
 }
